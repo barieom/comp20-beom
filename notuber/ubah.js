3fd6bc57a007df33a8d23a27ae4ab0adad3157d7
Barry Eom <31739208+barieom@users.noreply.github.com>
2018-02-27 23:58:43 -0500

    implemented the basic XML

--- notuber/ubah.js
+++ notuber/ubah.js
@@ -1,14 +1,39 @@
 var myLat = 0;
 var myLng = 0;
 // var me = new google.maps.LatLng(myLat, myLng);
-var map;
+
 var marker;
 // var infowindow = new google.maps.InfoWindow();
+
+
+function requestData(self) {
+	request = new XMLHttpRequest();
+	request.open("POST", "https://jordan-marsh.herokuapp.com/rides", true);
+	request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			console.log("hey");
+			msg = document.getElementById("json_data");
+			ride_data = JSON.parse(request.responseText);
+			console.log(ride_data);
+			_msg = ""
+
+			for (i = 0; i < ride_data.length; i++) {
+				_msg += ride_data[i].content;
+				console.log(ride_data[i].content)
+			}
+			msg.innerHTML = _msg;
+		}
+	}
+
+	request.send("username=" + self.username + "&lat=" + self.lat + "&lng=" + self.lng);
+}
+
 
 function initMap() {
   "use strict";
   var mass = new google.maps.LatLng(42.4072, -71.3824),
-    map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
       center: mass,
       zoom: 8
     });
@@ -20,23 +45,23 @@ function initMap() {
     //   self: {
     //   }
     // },
-    // self = {
-    //   username: 'HTRHha9MYl',
-    //   lat: 0,
-    //   lng: 0,
-    //   position: new google.maps.LatLng(0, 0),
-    //   type: "self"
-    // };
-    getMyLocation(map);
+    self = {
+      username: 'HTRHha9MYl',
+      lat: 0,
+      lng: 0,
+      position: new google.maps.LatLng(0, 0),
+      type: "self"
+    };
+    getMyLocation(map, self);
 }
 
-function getMyLocation(map) {
+function getMyLocation(map, self) {
 	if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
 		navigator.geolocation.getCurrentPosition(function(position) {
-			myLat = position.coords.latitude;
-			myLng = position.coords.longitude;
-			console.log(myLat);
-			renderMap(map);
+			self.lat = position.coords.latitude;
+			self.lng = position.coords.longitude;
+			self.position = new google.maps.LatLng(self.lat, self.lng);
+			renderMap(map, self);
 		});
 	}
 	else {
@@ -44,15 +69,13 @@ function getMyLocation(map) {
 	}
 }
 
-function renderMap(map) {
-	me = new google.maps.LatLng(myLat, myLng);
-
+function renderMap(map, self) {
 	// Update map and go there...
-	map.panTo(me);
+	map.panTo(self);
 	
 	// Create a marker
 	marker = new google.maps.Marker({
-		position: me,
+		position: self,
 		title: "Here I Am!"
 	});
 	marker.setMap(map);
@@ -62,4 +85,5 @@ function renderMap(map) {
 		infowindow.setContent(marker.title);
 		infowindow.open(map, marker);
 	});
+	requestData(self);
 }
